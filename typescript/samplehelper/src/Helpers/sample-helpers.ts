89f99253ee128c542bf1e3c30383d09fe3915bfc
Trevor Fayas <47198675+KenticoDevTrev@users.noreply.github.com>
2021-06-18 10:48:27 -0500

    Update sample-helpers.ts
    Adjusted the GetRequest

--- typescript/samplehelper/src/Helpers/sample-helpers.ts
+++ typescript/samplehelper/src/Helpers/sample-helpers.ts
@@ -21,7 +21,15 @@ export class SampleHelper {
         return await response.json();
     }
 
-    static async getRequest<T>(Url: string, Data?: any): Promise<T> {
+    static async getRequest<T>(Url: string, Data?: Dictionary<string, string>): Promise<T> {
+        // untested
+        let UrlWithParams = Url;
+        if(Data) {
+            const Params = Array<string>();
+              for(var key in Data)
+                 Params.push(encodeURIComponent(key) + "=" + encodeURIComponent(Data[key]));
+              UrlWithParams += "?"+ Params.join("&");
+        }
         const response = await fetch(Url, {
             method: "GET",
             mode: 'cors',
